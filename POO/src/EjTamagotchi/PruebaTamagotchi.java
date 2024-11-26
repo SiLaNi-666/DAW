e77e788b6af8966d972c0621a248282a92b8eee7
SiLaNi-666 <itzdanielgraciac666@gmail.com>
2024-11-26 12:24:41 +0100

    Create PruebaTamagotchi.java

---
+++ POO/src/EjTamagotchi/PruebaTamagotchi.java
@@ -0,0 +1,14 @@
+package Tamagotchi;
+
+public class PruebaTamagotchi {
+
+	public static void main (String[] args) {
+		
+		Tamagotchi t = new Tamagotchi ("Francisco", 5);
+		
+		t.come();
+		t.juega();
+	
+	}
+	
+}
